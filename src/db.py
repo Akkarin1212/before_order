c01656eaaddb64f4c895d99341a0aae99078bfcb
kmju1997 <kmju1997@gmail.com>
2018-12-01 20:21:15 +0900

    changed query for the dishname
    handled the dish name which has ingredient name in front of it

--- src/db.py
+++ src/db.py
@@ -27,14 +27,28 @@ def get_dishes(mydb, ko_dishes):
     cursor = mydb.cursor(buffered=True,dictionary=True)
     infos = []
     for ko_dish in ko_dishes:
-        query = ("SELECT dish.name, dish.description, ko_en.name as 'ko_name' "
-                "FROM dish, ko_en "
+        query = ("SELECT dish.name, dish.description, ko_en.name as 'ko_name', LOCATE(ko_en.name, '{0}') AS 'pos', LENGTH(ko_en.name) AS 'len', x.len, x.pos "
+                "FROM dish, ko_en left outer join (SELECT LOCATE(ko_en.name, '{0}') AS 'pos', "
+                "MAX(LENGTH(ko_en.name)) AS 'len' "
+                "FROM ko_en "
+                "Group by pos "
+                "HAVING pos != 0) x on x.len = len "
                 "WHERE dish.id = ko_en.dish_id "
-                "AND ko_en.name = '{0}' "
-                "LIMIT 1".format(ko_dish))
-
+                "HAVING pos != 0 AND x.len = len AND x.pos = pos "
+                "ORDER BY pos".format(ko_dish))
+        print(query)
         cursor.execute(query)
         for row in cursor:
+                
             infos.append(row)
     cursor.close()
+
+    if len(infos) > 0 :
+        pass
+        #handle for multiple results
+
+
+
+
+    #if it has one or zero directly returns infos 
     return infos
