bf500e39b5b5db5d0385b81f32186a8a10fc58d2
Kang Min Ju <kmju1997@gmail.com>
2019-06-12 23:39:10 +0900

    Update db.py

--- src/db.py
+++ src/db.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 def connect_db():
     mydb = mysql.connector.connect(
-    host="beforeordermysqlserver.mysql.database.azure.com",
-    user="goalkeeper@beforeordermysqlserver",
-    password="Christian0118!",
+    host="",
+    user="",
+    password="",
     database="food",
     ssl_ca="ssl_certificiate.pem"
     )
